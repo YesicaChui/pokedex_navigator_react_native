2ad0893e697d6590ec97287ff7b326908a463894
YesicaChui <yesicachuic@gmail.com>
2024-01-21 18:25:32 -0500

    tomando foto con camara

--- src/Screens/Perfil.js
+++ src/Screens/Perfil.js
@@ -1,14 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Image, View } from 'react-native'
+import AddButton from '../Components/AddButton'
+import * as ImagePicker from 'expo-image-picker'
 
 const Perfil = () => {
+
+  const [image, setImage] = useState("")
+
+  useEffect(() => {
+  }, [])
+
+  const pickImage = async () => {
+
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync()
+
+    if (granted) {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 0.3,
+        base64: true
+      })
+
+      if (!result.canceled) {
+        setImage('data:image/jpeg;base64,' + result.assets[0].base64)
+      }
+    }
+
+  }
+
+
   return (
-    <View>
-      <Text>Perfil</Text>
+    <View style={styles.container}>
+      <Image
+        source={image ? { uri: image } : require("../../assets/user.png")}
+        style={styles.image}
+        resizeMode='cover'
+
+      />
+      <AddButton title="Tomar foto" onPress={pickImage} />
     </View>
   )
 }
 
 export default Perfil
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20
+  },
+  image: {
+    width: 200,
+    height: 200
+  },
+  text: {
+
+
+  }
+})
