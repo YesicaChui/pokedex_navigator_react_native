e143fadb798f7b20ffa2f7bc5a89a049cf43e075
YesicaChui <yesicachuic@gmail.com>
2024-02-08 19:13:59 -0500

    agregando boton guardar favoritos

--- src/Screens/FavoritosPokemons.js
+++ src/Screens/FavoritosPokemons.js
@@ -1,24 +1,39 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { Button, FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 import PokemonCard from '../Components/PokemonCard'
 import { useSelector } from 'react-redux'
 
 const FavoritosPokemons = () => {
-  const PokemonFavoritos = useSelector((state)=>state.favoritos.value)
+  const PokemonFavoritos = useSelector((state) => state.favoritos.value)
+
+  const onPress = () => {
+
+  }
   return (
-    <FlatList
-    style={styles.container}
-    data={PokemonFavoritos}
-    numColumns={2}
-    keyExtractor={item => item.num}
-    renderItem={({ item }) => <PokemonCard pokemon={item}/>}
-  />
+    <>
+      <View style={styles.container2}>
+        <Button onPress={onPress} title='Guardar favoritos' color={'orange'} />
+      </View>
+      <FlatList
+        style={styles.container}
+        data={PokemonFavoritos}
+        numColumns={2}
+        keyExtractor={item => item.num}
+        renderItem={({ item }) => <PokemonCard pokemon={item} />}
+      />
+
+    </>
   )
 }
 
 export default FavoritosPokemons
 
 const styles = StyleSheet.create({
-  container:{
-    marginBottom:100
-  }
+  container: {
+    marginBottom: 100
+  },
+  container2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10
+  },
 })
